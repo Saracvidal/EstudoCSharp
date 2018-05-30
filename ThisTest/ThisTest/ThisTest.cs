495d12c1477ec102fa2822418adb8b2e464814f6
Sara Vidal de Souza <s.vidaldesouza@summaview.nl>
2018-05-30 14:51:36 +0200

    Commit ThisTest

---
+++ ThisTest/ThisTest/ThisTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThisTest
+{
+    class ThisTest
+    {
+        static void Main(string[] args)
+        {
+            Time4 time = new Time4(12, 30, 19);
+            Console.WriteLine(time.BuildString());
+        }
+    }
+}
---
+++ ThisTest/ThisTest/Time4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThisTest
+{
+    public class Time4
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time4(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public string BuildString()
+        {
+            return "this.ToStandardString(): " + this.ToStandardString();
+        }
+
+        public string ToStandardString()
+        {
+            return String.Format("{0}:{1:D2}:{2:D2} {3}", ((this.hour == 12 || this.hour == 0) ? 12 : this.hour % 12), this.minute, this.second, (this.hour < 12 ? "AM" : "PM"));
+        }
+    }
+}
