c5c72640de9206e62c837884ea043fad17a130d6
Sara Vidal de Souza <s.vidaldesouza@summaview.nl>
2018-05-30 15:48:56 +0200

    Static Test

---
+++ StaticTest/StaticTest/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticTest
+{
+    public class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private static int count;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            ++count;
+
+            Console.WriteLine("Employee object constructor: " + firstName + " " + lastName + ", Count = " + Count);
+        }
+
+        //Destructor decrements Employee static count
+        ~Employee()
+        {
+            --count;
+            Console.WriteLine("Employee object destructor: " + firstName + " " + lastName + ", Count = " + Count);
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+        }
+
+        public static int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+    }
+}
---
+++ StaticTest/StaticTest/StaticTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticTest
+{
+    class StaticTest
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Employees before sntantiation: " + Employee.Count + "\n");
+
+            Employee employee1 = new Employee("Suzan", "Baker");
+            Employee employee2 = new Employee("Bob", "Jhones");
+
+            Console.WriteLine("Employees after sntantiation: " + "Employee.Count:" + Employee.Count + "\n");
+
+            Console.WriteLine("Employee 1: " + employee1.FirstName + " " + employee1.LastName + "\n" + "Employee 2: " + employee2.FirstName + " " + employee2.LastName + "\n");
+
+            employee1 = null;
+            employee2 = null;
+
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("\nEmployees after garbage collection: " + Employee.Count);
+        }
+    }
+}
