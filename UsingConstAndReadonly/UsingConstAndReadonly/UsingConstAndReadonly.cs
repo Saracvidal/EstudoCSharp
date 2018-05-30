134d9c45e1c3ae5b4e9a2e39f13607b6ad040d6e
Sara Vidal de Souza <s.vidaldesouza@summaview.nl>
2018-05-30 16:05:53 +0200

    Using Constants and ReadOnly

---
+++ UsingConstAndReadonly/UsingConstAndReadonly/UsingConstAndReadonly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UsingConstAndReadonly
+{
+    public class Constants
+    {
+        public const double PI = 3.14159;
+        public readonly int radius;
+
+        public Constants(int radius)
+        {
+            this.radius = radius;
+        }
+    }
+
+    public class UsingConstAndReadonly
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Constants constantValues = new Constants(random.Next(1, 20));
+
+            Console.WriteLine("Radius: " + constantValues.radius + "\nCircumference: " + constantValues.radius * Constants.PI * 2);
+        }
+    }
+}
